74b0df2877ba96fe4b5035aa419572b6ba3cb8fb
ELAPOTP <artyom.suhlyak@gmail.com>
2013-09-10 17:40:57 +0300

    Testing commiting from II

---
+++ src/edu/mmor/l1/test.java
@@ -0,0 +1,11 @@
+package edu.mmor.l1;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Artyom
+ * Date: 10.09.13
+ * Time: 17:40
+ * To change this template use File | Settings | File Templates.
+ */
+public class test {
+}
