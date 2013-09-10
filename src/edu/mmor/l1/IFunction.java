941e94ff7b0fd1f3c1e1f87304bac4292cb1e6be
ELAPOTP <artyom.suhlyak@gmail.com>
2013-09-10 18:02:15 +0300

    Reupdate all project

---
+++ src/edu/mmor/l1/IFunction.java
@@ -0,0 +1,11 @@
+package edu.mmor.l1;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Artyom
+ * Date: 10.09.13
+ * Time: 18:01
+ * To change this template use File | Settings | File Templates.
+ */
+public interface IFunction {
+}
