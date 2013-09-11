a37a0bf66299878631d36837aea70077890dbe6b
DTSHNICK <dtshnick@gmail.com>
2013-09-11 10:57:18 +0300

    Создал реализацию нахождения всех функци; Сделал абстрактный класс к функциям, накатал немного форму"; Реализован "АЗС"

--- src/edu/mmor/l1/Functions/Function10.java
+++ src/edu/mmor/l1/Functions/Function10.java
@@ -11,7 +11,7 @@ public class Function10 extends Function {
     @Override
     public double calculate(double x) {
         double result;
-        result=-Math.pow(x,2)*(x+1)*Math.pow(x-2,1/3);
+        result=-Math.pow(x, 2.0) * (x+1.0) * ((x - 2 < 0) ? -Math.pow(2 - x, 1.0/3.0) : Math.pow(x-2, 1.0/3.0));
         return result;
     }
 }
